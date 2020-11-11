07e9bf73dc4920b90c3f68f9a28c4a4c0b8c19d4
Joseph Budina <joebud3@gmail.com>
2020-11-11 10:14:39 -0700

    Add total_inventory test

--- test/market_test.rb
+++ test/market_test.rb
@@ -110,4 +110,78 @@ class MarketTest < Minitest::Test
     assert_equal 345.00, vendor2.potential_revenue 
     assert_equal 48.75, vendor3.potential_revenue 
   end
+  
+  def test_total_inventory
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(@item1, 35)
+
+    vendor1.stock(@item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+  
+
+    vendor2.stock(@item4, 50)
+
+    vendor2.stock(@item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    
+
+    vendor3.stock(@item1, 65)
+
+    @market.add_vendor(vendor1)
+
+    @market.add_vendor(vendor2)
+
+    @market.add_vendor(vendor3)
+
+    expected = {
+      @item1 => {
+        quantity: 100,
+        vendors: [vendor1, vendor3]
+      },
+      @item2 => {
+        quantity: 7,
+        vendors: [vendor1]
+      },
+      @item4 => {
+        quantity: 50,
+        vendors: [vendor2]
+      },
+      @item3 => {
+        quantity: 35,
+        vendors: [vendor2, vendor3]
+      },
+    }
+
+    assert_equal expected, @market.total_inventory
+  end
+  
+  def test_sorted_item_list
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(@item1, 35)
+
+    vendor1.stock(@item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+  
+
+    vendor2.stock(@item4, 50)
+
+    vendor2.stock(@item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    
+
+    vendor3.stock(@item1, 65)
+
+    @market.add_vendor(vendor1)
+
+    @market.add_vendor(vendor2)
+
+    @market.add_vendor(vendor3)
+
+    expected = ["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"]
+    assert_equal expected, @market.sorted_item_list
+  end
 end
