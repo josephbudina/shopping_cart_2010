a9c8abfe13c1d262e788ef5238dffcdee506bb7e
Joseph Budina <joebud3@gmail.com>
2020-11-11 09:24:35 -0700

    Add stock test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -16,4 +16,24 @@ class VendorTest < Minitest::Test
     assert_equal "Rocky Mountain Fresh", @vendor.name 
     assert_equal ({}), @vendor.inventory 
   end
+
+  def test_it_can_add_stock
+    assert_equal ({}), @vendor.inventory
+    
+    assert_equal 0, @vendor.check_stock(@item1)
+    
+    @vendor.stock(@item1, 30)
+    
+    assert_equal ({@item1 => 30}) ,@vendor.inventory
+    
+    assert_equal 30, @vendor.check_stock(@item1)
+    
+    @vendor.stock(@item1, 25)
+    
+    assert_equal 55, @vendor.check_stock(@item1)
+    
+    @vendor.stock(@item2, 12)
+
+    assert_equal ({@item1 => 55, @item2 => 12}), @vendor.inventory
+  end
 end
