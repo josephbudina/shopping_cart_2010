daeac32b081296a105a8403b302033509e887843
Joseph Budina <joebud3@gmail.com>
2020-11-11 09:47:17 -0700

    Add market names test

--- test/market_test.rb
+++ test/market_test.rb
@@ -50,4 +50,33 @@ class MarketTest < Minitest::Test
 
     assert_equal expected, @market.vendors
   end
+
+  def test_vendors_names
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(@item1, 35)
+
+    vendor1.stock(@item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+   
+
+    vendor2.stock(@item4, 50)
+
+    vendor2.stock(@item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    
+
+    vendor3.stock(@item1, 65)
+
+    @market.add_vendor(vendor1)
+
+    @market.add_vendor(vendor2)
+
+    @market.add_vendor(vendor3)
+
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+
+    assert_equal expected, @market.names 
+  end
 end
