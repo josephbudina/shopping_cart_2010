952ba2cae44842cf689ec17b7fc40dd0cde3df4d
Joseph Budina <joebud3@gmail.com>
2020-11-11 10:01:52 -0700

    Add revenue test

--- test/market_test.rb
+++ test/market_test.rb
@@ -81,4 +81,33 @@ class MarketTest < Minitest::Test
     assert_equal expected1, @market.vendors_that_sell(@item1)
     assert_equal expected2, @market.vendors_that_sell(@item4)
   end
+  
+  def test_potential_revenuee 
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    vendor1.stock(@item1, 35)
+
+    vendor1.stock(@item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+  
+
+    vendor2.stock(@item4, 50)
+
+    vendor2.stock(@item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    
+
+    vendor3.stock(@item1, 65)
+
+    @market.add_vendor(vendor1)
+
+    @market.add_vendor(vendor2)
+
+    @market.add_vendor(vendor3)
+
+    assert_equal 29.75, @vendor1.potential_revenue 
+    assert_equal 345.00, @vendor2.potential_revenue 
+    assert_equal 48.75, @vendor3.potential_revenue 
+  end
 end
